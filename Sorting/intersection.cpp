d2222a76780334a39219ec9c8f2a650db883d0af
Aman <amanmalik3105@gmail.com>
2024-03-18 19:27:15 +0530

    merge_sort_solution

--- Sorting/intersection.cpp
+++ Sorting/intersection.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void intersection(int a[], int b[], int m, int n){
+
+int i = 0 ; int j = 0 ;
+
+while(i < m && j < n){
+    if(i > 0 && a[i] == a[i-1]){i++; continue;}
+    if(a[i] > b[j]) { j++; }
+    else if(a[i] < b[j]) {i++;}
+    else {cout << a[i] << " " ; i++ ; j++;}
+}
+
+}
+
 void naive_intersection(int arr1[], int arr2[] , int n1 , int n2){
     for(int i = 0 ; i < n1 ; i++)
     {
@@ -20,6 +33,8 @@ int main() {
     int array1[5] = {1, 20, 20, 40, 60};
     int array2[4] = {2, 20, 20, 20};
 
-    naive_intersection(array1, array2, 5, 4) ;
+    // naive_intersection(array1, array2, 5, 4) ;
+
+    intersection(array1 , array2 , 5, 4);
 
 }
