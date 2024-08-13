f9dd404ee10d279c7ed44aa757d1fa6b6d5c0750
amwn1 <amanmalik3105@gmail.com>
2024-08-13 09:19:49 +0530

    demo

---
+++ Hashing/implementation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<string, int> m; // creation of the map 
+    
+    // insetion of the map 
+    pair<string,int> p = make_pair("aman",1);
+    m.insert(p);
+    
+    //second way of insertion
+
+    pair<string, int> pair2("malik",2);
+    m.insert(pair2);
+
+    //third way of insertion
+
+    m["kumar"] = 3; // this will insert the value 3 with the key kumar
+
+    return 0;
+}
