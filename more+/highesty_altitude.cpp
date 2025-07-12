7b7eca7866658dd39f3a4568a07e2cd5839a4251
Aman <amanmalik3105@gmail.com>
2025-07-12 11:21:16 +0530

    new boiler plate

---
+++ more+/highesty_altitude.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    // Paste the function here()
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
